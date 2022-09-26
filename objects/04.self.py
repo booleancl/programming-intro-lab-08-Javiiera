4558286f79dc015c7b607f30f18daa2d513b7b26
Javiera <javiera19.navarrete@gmail.com>
2022-09-26 10:55:10 -0300

    Diccionario

--- objects/04.self.py
+++ objects/04.self.py
@@ -3,3 +3,18 @@
 # palabra reservada class
 # 
 
+
+class student:
+    def __init__(self,name,position,skills):
+        self.name = name
+        self.position = position
+        self.skills = skills
+    def say_name(self):
+        print("mi nombre es",self.name,"mi cargo es ",self.position,"mis habilidades son",self.skills)
+
+Alice = student("Alice","fullstack Developer",["Python","Git","HTML","CSS","javascript"])
+Bob_el_chef = student("Bob el chef","kitchen helper",["Pastry chet international and cake shop","international foof","sea food"])
+
+Alice.say_name()
+
+Bob_el_chef.say_name()
